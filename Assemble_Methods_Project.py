e71a8e9d856af56283ef78a30256ed48c7f22f9f
Maciej Pająk <mpajak98@gmail.com>
2022-04-12 19:07:14 +0200

    todo list

--- Assemble_Methods_Project.py
+++ Assemble_Methods_Project.py
@@ -1,4 +1,14 @@
-import sklearn
+from operator import delitem
+import numpy as np
+import pandas
+from sklearn.model_selection import KFold
 
-if __name__ == "main":
-    print("Hello")
+
+#  1 Przygotowanie danych- napisać funkcję, które będzie importować dane z plików do klasyfikatorów
+#  2 Wybór i konfiguracja odpowiednich klasyfikatorów
+#  3 Główna pętla ucząca
+#  4 Zbieranie danych wynikowych uczenia + eksport do csv
+#  5 Testy statystyczne
+#  6 Wizualizacja
+
+
