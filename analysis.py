c630595cb6768cacbe6457268f0d01df03b40c98
Krzysztof Ragan <krzysztofragan1999@gmail.com>
2022-04-12 23:08:15 +0200

    Statistic tests added

--- analysis.py
+++ analysis.py
@@ -1,4 +1,12 @@
 import numpy as np
+from scipy.stats import rankdata
+from scipy.stats import ranksums
+from main import clfs
+from tabulate import tabulate
+from scipy.stats import ttest_rel
+
+
+
 
 
 scores = np.load('results.npy')
@@ -7,3 +15,92 @@ print("\nScores:\n", scores.shape)
 mean_scores = np.mean(scores, axis = 2).T
 print("\nMean scores: \n", mean_scores)
 
+#TEST FRIEDMANNA
+
+# ranks = []
+# for ms in mean_scores:
+#     ranks.append(rankdata(ms).tolist())
+# ranks = np.array(ranks)
+# print("\nRanks:\n", ranks)
+
+# mean_ranks = np.mean(ranks, axis=0)
+# print("\nMean ranks:\n", mean_ranks)
+
+################   TESTY PAROWE   ###################
+
+
+#TEST WILCOXONA
+
+# alfa = .05
+# w_statistic = np.zeros((len(clfs), len(clfs)))
+# p_value = np.zeros((len(clfs), len(clfs)))
+
+# for i in range(len(clfs)):
+#     for j in range(len(clfs)):
+#         w_statistic[i, j], p_value[i, j] = ranksums(ranks.T[i], ranks.T[j])
+
+
+headers = list(clfs.keys())
+names_column = np.expand_dims(np.array(list(clfs.keys())), axis=1)
+# w_statistic_table = np.concatenate((names_column, w_statistic), axis=1)
+# w_statistic_table = tabulate(w_statistic_table, headers, floatfmt=".2f")
+# p_value_table = np.concatenate((names_column, p_value), axis=1)
+# p_value_table = tabulate(p_value_table, headers, floatfmt=".2f")
+# print("\nw-statistic:\n", w_statistic_table, "\n\np-value:\n", p_value_table)
+
+# advantage = np.zeros((len(clfs), len(clfs)))
+# advantage[w_statistic > 0] = 1
+# advantage_table = tabulate(np.concatenate(
+#     (names_column, advantage), axis=1), headers)
+# print("\nAdvantage:\n", advantage_table)
+
+# significance = np.zeros((len(clfs), len(clfs)))
+# significance[p_value <= alfa] = 1
+# significance_table = tabulate(np.concatenate(
+#     (names_column, significance), axis=1), headers)
+# print("\nStatistical significance (alpha = 0.05):\n", significance_table)
+
+# stat_better = significance * advantage
+# stat_better_table = tabulate(np.concatenate(
+#     (names_column, stat_better), axis=1), headers)
+# print("Statistically significantly better:\n", stat_better_table)
+
+#TEST T-STUDENTA
+
+mean_scores = np.mean(scores, axis = 2)
+print("\nMean scores: \n", mean_scores)
+
+
+
+alfa = .05
+t_statistic = np.zeros((len(clfs), len(clfs)))
+p_value = np.zeros((len(clfs), len(clfs)))
+
+for i in range(len(clfs)):
+    for j in range(len(clfs)):
+        t_statistic[i, j], p_value[i, j] = ttest_rel(scores[i], scores[j])
+print("t-statistic:\n", t_statistic, "\n\np-value:\n", p_value)
+
+
+t_statistic_table = np.concatenate((names_column, t_statistic), axis=1)
+t_statistic_table = tabulate(t_statistic_table, headers, floatfmt=".2f")
+p_value_table = np.concatenate((names_column, p_value), axis=1)
+p_value_table = tabulate(p_value_table, headers, floatfmt=".2f")
+print("t-statistic:\n", t_statistic_table, "\n\np-value:\n", p_value_table)
+
+advantage = np.zeros((len(clfs), len(clfs)))
+advantage[t_statistic > 0] = 1
+advantage_table = tabulate(np.concatenate(
+    (names_column, advantage), axis=1), headers)
+print("\nAdvantage:\n", advantage_table)
+
+significance = np.zeros((len(clfs), len(clfs)))
+significance[p_value <= alfa] = 1
+significance_table = tabulate(np.concatenate(
+    (names_column, significance), axis=1), headers)
+print("\nStatistical significance (alpha = 0.05):\n", significance_table)
+
+stat_better = significance * advantage
+stat_better_table = tabulate(np.concatenate(
+    (names_column, stat_better), axis=1), headers)
+print("Statistically significantly better:\n", stat_better_table)
--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from sklearn.ensemble import AdaBoostClassifier, BaggingClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.base import clone
 from sklearn.metrics import accuracy_score
-from sklearn.naive_bayes import GaussianNB
 
 RANDOM_STATE = 1234
 BASE_CLF = DecisionTreeClassifier(random_state = RANDOM_STATE)
@@ -23,12 +22,10 @@ N_ESTIMATORS = 50
 clfs = {
     'ABOO': AdaBoostClassifier(base_estimator = BASE_CLF, n_estimators = N_ESTIMATORS, random_state = RANDOM_STATE),
     'BAG': BaggingClassifier(base_estimator = BASE_CLF, n_estimators = N_ESTIMATORS, random_state = RANDOM_STATE),
-    'GNB': GaussianNB(),
 }
 
 
 datasets = ['ecoli-0_vs_1','ecoli1', 'ecoli2', 'ecoli3', 'glass-0-1-2-3_vs_4-5-6', 'glass0', 'glass1', 'glass6', 'haberman', 'iris0', 'new-thyroid1', 'newthyroid2', 'page-blocks0', 'pima', 'segment0', 'vehicle0', 'vehicle1', 'vehicle2', 'vehicle3', 'wisconsin', 'yeast1', 'yeast3']
-
 
 n_datasets = len(datasets)
 n_splits = 5
@@ -39,10 +36,9 @@ scores = np.zeros((len(clfs), n_datasets, n_splits * n_repeats))
 
 
 for data_id, dataset in enumerate(datasets):
-    dataset = np.genfromtxt("datasets/%s.dat" % (dataset), delimiter=",", comments = '@', converters = {(-1): lambda s: 0.0 if s.decode('ascii') == 'negative' else 1.0} )
+    dataset = np.genfromtxt("datasets/%s.dat" % (dataset), delimiter=",", comments = '@', converters = {(-1): lambda s: 0.0 if (s.strip().decode('ascii')) == 'negative' else 1.0} )
     X = dataset[:, :-1]
     y = dataset[:, -1].astype(int)
-    print(X , y)
 
     for fold_id, (train, test) in enumerate(rskf.split(X, y)):
         for clf_id, clf_name in enumerate(clfs):
