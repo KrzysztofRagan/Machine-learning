05a8668ec8be46857f2cafad0f1ffb68cb67620b
Maciej Pająk <mpajak98@gmail.com>
2022-05-04 11:16:56 +0200

    new estimators

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import numpy as np
-from sklearn.model_selection import RepeatedStratifiedKFold
+from sklearn.model_selection import RepeatedStratifiedKFold, cross_val_score
 from sklearn.base import clone
 from sklearn.metrics import accuracy_score
 from parameters import *
@@ -8,17 +8,31 @@ rskf = RepeatedStratifiedKFold(n_splits=n_splits, n_repeats=n_repeats, random_st
 
 scores = np.zeros((len(clfs), n_datasets, n_splits * n_repeats))
 
+# for data_id, dataset in enumerate(datasets):
+#     dataset = np.genfromtxt("datasets/%s.dat" % dataset, delimiter=",", comments='@',
+#                             converters={(-1): lambda s: 0.0 if (s.strip().decode('ascii')) == 'negative' else 1.0})
+#     X = dataset[:, :-1]
+#     y = dataset[:, -1].astype(int)
+#
+#     for fold_id, (train, test) in enumerate(rskf.split(X, y)):
+#         for clf_id, clf_name in enumerate(clfs):
+#             clf = clone(clfs[clf_name])
+#             clf.fit(X[train], y[train])
+#             y_pred = clf.predict(X[test])
+#             scores[clf_id, data_id, fold_id] = accuracy_score(y[test], y_pred)
+#
+# np.save('results', scores)
+# print(scores)
+
 for data_id, dataset in enumerate(datasets):
     dataset = np.genfromtxt("datasets/%s.dat" % dataset, delimiter=",", comments='@',
                             converters={(-1): lambda s: 0.0 if (s.strip().decode('ascii')) == 'negative' else 1.0})
     X = dataset[:, :-1]
     y = dataset[:, -1].astype(int)
 
-    for fold_id, (train, test) in enumerate(rskf.split(X, y)):
-        for clf_id, clf_name in enumerate(clfs):
-            clf = clone(clfs[clf_name])
-            clf.fit(X[train], y[train])
-            y_pred = clf.predict(X[test])
-            scores[clf_id, data_id, fold_id] = accuracy_score(y[test], y_pred)
+    for clf_id, clf_name in enumerate(clfs):
+        clf = clfs[clf_name]
+        scores[clf_id, data_id] = cross_val_score(clf, X, y, scoring='accuracy', cv=rskf, n_jobs=-1)
 
 np.save('results', scores)
+print(scores)
--- parameters.py
+++ parameters.py
@@ -1,24 +1,55 @@
 from sklearn.ensemble import AdaBoostClassifier, BaggingClassifier
 from sklearn.tree import DecisionTreeClassifier
+from imblearn.pipeline import Pipeline
+from imblearn.over_sampling import SMOTE, BorderlineSMOTE
+from imblearn.under_sampling import RandomUnderSampler
+from imblearn.ensemble import RUSBoostClassifier
 
 RANDOM_STATE = 1234
 BASE_CLF = DecisionTreeClassifier(random_state=RANDOM_STATE)
 
 N_ESTIMATORS = 50
 
+# clfs = {
+#     'ABOO': AdaBoostClassifier(base_estimator=BASE_CLF, n_estimators=N_ESTIMATORS, random_state=RANDOM_STATE),
+#     'BAG': BaggingClassifier(base_estimator=BASE_CLF, n_estimators=N_ESTIMATORS, random_state=RANDOM_STATE),
+#     'SMOTE': [('over', SMOTE()), ('model', BASE_CLF)]
+# }
+
+
 clfs = {
-    'ABOO': AdaBoostClassifier(base_estimator=BASE_CLF, n_estimators=N_ESTIMATORS, random_state=RANDOM_STATE),
-    'BAG': BaggingClassifier(base_estimator=BASE_CLF, n_estimators=N_ESTIMATORS, random_state=RANDOM_STATE),
+    'ABOO': Pipeline(steps=[('model', AdaBoostClassifier(base_estimator=BASE_CLF,
+                                                         n_estimators=N_ESTIMATORS,
+                                                         random_state=RANDOM_STATE))]),
+    'BAG': Pipeline(steps=[('model', BaggingClassifier(base_estimator=BASE_CLF,
+                                                       n_estimators=N_ESTIMATORS,
+                                                       random_state=RANDOM_STATE))]),
+    'SMOTE': Pipeline(steps=[('over', SMOTE()), ('model', AdaBoostClassifier(base_estimator=BASE_CLF,
+                                                                             n_estimators=N_ESTIMATORS,
+                                                                             random_state=RANDOM_STATE))]),
+    'BorderlineSMOTE': Pipeline(
+        steps=[('over', BorderlineSMOTE()), ('model', AdaBoostClassifier(base_estimator=BASE_CLF,
+                                                                         n_estimators=N_ESTIMATORS,
+                                                                         random_state=RANDOM_STATE))]),
+    'SMOTE+UNDER': Pipeline(steps=[('over', SMOTE()),
+                                   ('under', RandomUnderSampler()),
+                                   ('model', AdaBoostClassifier(base_estimator=BASE_CLF,
+                                                                n_estimators=N_ESTIMATORS,
+                                                                random_state=RANDOM_STATE))]),
+    'RUSBoost': Pipeline(steps=[('model', RUSBoostClassifier(base_estimator=BASE_CLF,
+                                                             n_estimators=N_ESTIMATORS,
+                                                             random_state=RANDOM_STATE))]),
 }
 
 datasets = ['ecoli-0_vs_1', 'ecoli1', 'ecoli2', 'ecoli3', 'glass-0-1-2-3_vs_4-5-6', 'glass0', 'glass1', 'glass6',
             'haberman', 'iris0', 'new-thyroid1', 'newthyroid2', 'page-blocks0', 'pima', 'segment0', 'vehicle0',
             'vehicle1', 'vehicle2', 'vehicle3', 'wisconsin', 'yeast1', 'yeast3']
 
+# datasets = ['ecoli-0_vs_1', 'ecoli1', 'ecoli2', 'ecoli3']
+
 n_datasets = len(datasets)
 n_splits = 5
 n_repeats = 2
-
 
 #  1 Przygotowanie danych- napisać funkcję, które będzie importować dane z plików do klasyfikatorów
 #  2 Wybór i konfiguracja odpowiednich klasyfikatorów
