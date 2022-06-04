fff1b913120c9a9f18fe6a6399f3a9d8ad9935d8
Maciej Pająk <mpajak98@gmail.com>
2022-06-04 18:21:28 +0200

    refactoring

--- main.py
+++ main.py
@@ -4,25 +4,23 @@ from parameters import *
 from experiment import experiment
 from analysis import *
 from datasets import datasets
-from sklearn.metrics import f1_score, accuracy_score
+from sklearn.metrics import f1_score, accuracy_score, balanced_accuracy_score, precision_score, recall_score
 from imblearn.metrics import geometric_mean_score, specificity_score
 from classifiers import clfs
 import pandas as pd
 
-# used metrics
-metrics = (geometric_mean_score, specificity_score, f1_score, accuracy_score)
-metrics_names = ('geometric_mean_score', 'specificity_score', 'f1_score', 'accuracy_score')
 
 # experiment loop
-# for clf_name, clf in clfs.items():
-#     print(clf_name)
-#     scores = experiment(clf=clf,
-#                         datasets=datasets,
-#                         metrics=metrics,
-#                         n_repeats=n_repeats,
-#                         n_splits=n_splits,
-#                         random_state=RANDOM_STATE)
-#     np.save(f'./results/{clf_name}', scores)  # save scores in clf_name.npy file
+def main_loop(clfs, datasets, metrics, n_repeats, n_splits, RANDOM_STATE):
+    for clf_name, clf in clfs.items():
+        print(clf_name)
+        scores = experiment(clf=clf,
+                            datasets=datasets,
+                            metrics=metrics,
+                            n_repeats=n_repeats,
+                            n_splits=n_splits,
+                            random_state=RANDOM_STATE)
+        np.save(f'./results/{clf_name}', scores)  # save scores in clf_name.npy file
 
 
 ###########################################################################
@@ -30,37 +28,38 @@ metrics_names = ('geometric_mean_score', 'specificity_score', 'f1_score', 'accur
 ###########################################################################
 
 
-clfs_names = [clf_name for clf_name in clfs.keys()]
-clfs_scores = {clf_name: np.load(f'./results/{clf_name}.npy') for clf_name in clfs_names}
+def analysis(clfs_names, result_dir, met_comp_dir):
+    clfs_scores = {}
+    clfs_mean_scores = {}
+    met_mean_scores = {}
 
-clfs_mean_scores = {clf_name: np.mean(clfs_scores[clf_name], axis=2) for clf_name in clfs_scores.keys()}
+    for clf_name in clfs_names:
+        clfs_scores[clf_name] = np.load(f'{result_dir}{clf_name}.npy')
+        clfs_mean_scores[clf_name] = np.mean(clfs_scores[clf_name], axis=2)
 
-# saving mean_scores to files (1 classfier = 1 file)
-for clf_name, mean_score in clfs_mean_scores.items():
-    mean_score = pd.DataFrame(np.round(mean_score.T, 2), columns=metrics_names, index=datasets)
-    mean_score.to_csv(f'./results/clfs_mean_scores/{clf_name}.csv', sep='\t')
+        # saving mean_scores to files (1 classfier = 1 file)
+        mean_score = pd.DataFrame(np.round(clfs_mean_scores[clf_name].T, 3), columns=metrics_names, index=datasets)
+        mean_score.to_csv(f'{result_dir}clfs_mean_scores/{clf_name}.csv', sep='\t')
 
-# saving metrics to files (1 metric = 1 file)
-met_mean_scores = {}
-for i, metric_name in enumerate(metrics_names):
-    mean_score = np.array([ms[i] for ms in clfs_mean_scores.values()])
-    met_mean_scores[metric_name] = mean_score
-    mean_score = pd.DataFrame(np.round(mean_score.T, 2), columns=clfs_names, index=datasets)
-    mean_score.to_csv(f'./results/metric_mean_scores/dist_comparison/{metric_name}.csv', sep='\t')
+    # saving metrics to files (1 metric = 1 file)
+    for i, metric_name in enumerate(metrics_names):
+        mean_score = np.array([ms[i] for ms in clfs_mean_scores.values()])
+        met_mean_scores[metric_name] = mean_score
+        mean_score = pd.DataFrame(np.round(mean_score.T, 3), columns=clfs_names, index=datasets)
+        mean_score.to_csv(f'{result_dir}metric_mean_scores/{met_comp_dir}{metric_name}.csv', sep='\t')
 
-# testing different parameters by t-student
-g_mean_score = np.array([s[0] for s in clfs_scores.values()])
-g_mean_scores = np.swapaxes(g_mean_score, 0, 1)  # datasets x metrics x folds
-print(g_mean_scores.shape)
+    # testing different parameters by t-student
+    g_mean_score = np.array([s[0] for s in clfs_scores.values()])
+    g_mean_scores = np.swapaxes(g_mean_score, 0, 1)  # datasets x metrics x folds
 
-stat_better_df = []
-for i, g_mean in enumerate(g_mean_scores):
-    # print('\n', datasets[i])
-    stat_better, stat_better_list = t_student(g_mean, clfs_names)
-    stat_better_df.append(stat_better_list)
+    stat_better_df = []
+    for i, g_mean in enumerate(g_mean_scores):
+        stat_better, stat_better_list = t_student(g_mean, clfs_names)
+        stat_better_df.append(stat_better_list)
 
-stat_better_df = pd.DataFrame(stat_better_df, columns=clfs_names, index=datasets)
-print(stat_better_df)
+    stat_better_df = pd.DataFrame(stat_better_df, columns=clfs_names, index=datasets)
+    print(stat_better_df.to_string())
+
 
 # scores = np.load('results.npy')
 # print("\nScores:\n", scores.shape)
@@ -78,3 +77,23 @@ print(stat_better_df)
 # #     t_student(dateset_scores)
 #
 # wilcoxon(ranks)
+
+
+if __name__ == '__main__':
+    # used metrics
+    metrics = (
+        geometric_mean_score, f1_score, balanced_accuracy_score, precision_score, recall_score, specificity_score)
+    # metrics_names = ("geometric_mean_score", "f1_score", "balanced_accuracy_score",
+    #                  "precision_score", "recall_score", "specificity_score")
+
+    metrics_names = ("geometric_mean_score", "specificity_score", "f1_score", "accuracy_score")
+
+    # main_loop(clfs, datasets, metrics, n_repeats, n_splits, RANDOM_STATE)
+
+
+
+    result_dir = "./results/"
+    met_comp_dir = "final_comparison/"
+    clfs_names = [clf_name for clf_name in clfs.keys()]
+
+    analysis(clfs_names, result_dir, met_comp_dir)
